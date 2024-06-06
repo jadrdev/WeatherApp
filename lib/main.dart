ae050e6bc5f05ada77f226874d35cf26464b50a3
Joshua A. Díaz Robayna <jdiarob@saosl.com>
2024-06-06 16:14:14 +0100

    feat: Add WeatherPage and update main.dart to use it

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/pages/weather_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+       home: WeatherPage());
   }
 }

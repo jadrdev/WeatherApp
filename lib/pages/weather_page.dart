c8c883c2c4fb64bc52482db9e4f7eab1ddbe87bb
Joshua A. Díaz Robayna <jdiarob@saosl.com>
2024-06-06 22:09:52 +0100

    feat: Add weather animation based on weather condition

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:weather_app/models/weather_model.dart';
 import 'package:weather_app/services/weather_service.dart';
 
@@ -10,7 +11,8 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
-  final _weatherService = WeatherService(apiKey: 'a6cb194a20e8d3b697f141f7cc1d8bd4');
+  final _weatherService =
+      WeatherService(apiKey: 'a6cb194a20e8d3b697f141f7cc1d8bd4');
   Weather? _weather;
 
   _fecthWeather() async {
@@ -26,6 +28,30 @@ class _WeatherPageState extends State<WeatherPage> {
     }
   }
 
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) 'assets/rain.json';
+
+    switch (mainCondition?.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/cloud.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/rain.json';
+      case 'thunderstorm':
+        return 'assets/thunder.json';
+      case 'clear':
+        return 'assets/sunny.json';
+      default:
+        return 'assets/sunny.json';
+    }
+  }
+
   @override
   void initState() {
     // TODO: implement initState
@@ -37,16 +63,47 @@ class _WeatherPageState extends State<WeatherPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-          Text(_weather?.cityName ?? "loading ..."),
-        
-          //Temperature
-          Text('${_weather?.temperature.round()}C'),
-        ],),
-      )
-    );
+        backgroundColor: Colors.grey[700],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                _weather?.cityName ?? "loading ...",
+                style: const TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+
+              //Weather Icon
+              Lottie.asset(getWeatherAnimation(_weather?.mainCodition)),
+
+              //Temperature
+              Text(
+                '${_weather?.temperature.round()}C',
+                style: const TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white
+                ),
+              ),
+
+              //weather condition
+              Text(
+                _weather?.mainCodition ?? "loading ...",
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
